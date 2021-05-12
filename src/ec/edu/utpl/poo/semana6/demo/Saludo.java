fb2916673fab5faea029d20ce39afa2149afea1f
IsaacAlvarez12 <isaaksalv12@gmail.com>
2021-05-12 17:56:27 -0500

    Actualizacion origen del saludo

--- src/ec/edu/utpl/poo/semana6/demo/Saludo.java
+++ src/ec/edu/utpl/poo/semana6/demo/Saludo.java
@@ -3,5 +3,6 @@ package ec.edu.utpl.poo.semana6.demo;
 public class Saludo {
     public static void main(String [] args){
         System.out.println("Hola Mundo");
+        System.out.println("Saludo desde Loja");
     }
 }
